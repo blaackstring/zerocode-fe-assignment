57dbd805d1869743c4f585e69f7405398a46bcd9
mohd shahan siddiqui <133672067+blaackstring@users.noreply.github.com>
2025-06-18 23:41:58 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   build: {
+    chunkSizeWarningLimit: 1000 // increase limit (default is 500)
+  },
   plugins: [react(), tailwindcss(),],
   server:{
     proxy:{
