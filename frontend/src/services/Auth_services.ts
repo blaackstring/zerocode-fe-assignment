184bdb8e3fe34224b02c78d73c1443ac90e764fd
mohd shahan siddiqui <133672067+blaackstring@users.noreply.github.com>
2025-06-20 15:43:47 +0530

    Update Auth_services.ts

--- frontend/src/services/Auth_services.ts
+++ frontend/src/services/Auth_services.ts
@@ -10,7 +10,7 @@ interface LogincredentialsProps{
 }
 
 
-const baseurl='https://chat-bot-bakend-git-main-blaackstrings-projects.vercel.app'
+const baseurl='https://n4vj10uc10.execute-api.ap-south-1.amazonaws.com'
 
 export const signup=async({username,email,password}:SignupcredentialsProps)=>{
     if(!email||!username||!password) return alert('All Fields Are Required')
